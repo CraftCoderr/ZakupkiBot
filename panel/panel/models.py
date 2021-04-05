dea0ae4ae7f638f50f33a7672385b4e920878752
tikhonk <tihon.klyshko@gmail.com>
2021-04-05 19:55:01 +0300

    Add new model and refactoring

--- panel/panel/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class Product(models.Model):
-    searchRequest = models.OneToOneField(SearchRequest, null=True, blank=True, on_delete=models.SET_NULL)
-
-
-class SearchRequest(models.Model):
-    product = models.OneToOneField(Product, null=True, blank=True, on_delete=models.SET_NULL)
